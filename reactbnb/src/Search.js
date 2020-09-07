48912186769f77d9401d67c3e97ecb3bf09e5fdc
Joshua Hartshorn <Pops@Joshuas-MacBook-Air-2.local>
2020-09-06 17:27:44 -0700

    Date picker added

--- reactbnb/src/Search.js
+++ reactbnb/src/Search.js
@@ -1,23 +1,32 @@
-import React, { useState } from 'react'
-import './Search.css'
-//Main Style file
-//import "react-data-range/dist/styles.css"
-//Theme CSS File
-//import "react-date-range/dist/theme/default.css"
-//import {DateRangePicker} from "react-date-range"
-
+import React, { useState } from "react";
+import { DateRangePicker } from "react-date-range";
+import "./Search.css";
+import "react-date-range/dist/styles.css"; // main style file
+import "react-date-range/dist/theme/default.css"; // theme css file
 
 // Date Picker Component
 function Search() {
-    const [startDate, setStartDate] = useState(new Date());
-    const [endDate, setEndDate] = useState(new Date());
+  const [startDate, setStartDate] = useState(new Date());
+  const [endDate, setEndDate] = useState(new Date());
 
+  //Required from the date range package
+  const selectionRange = {
+    startDate: new Date(),
+    endDate: new Date(),
+    key: "selection",
+  };
 
-    return (
-        <div className='search'>
-            
-        </div>
-    )
+  //Responsible for selecting a date. E.g. set the start date to what was selected for the start date
+  function handleSelect(ranges) {
+    setStartDate(ranges.selection.startDate);
+    setEndDate(ranges.selection.endDate);
+  }
+
+  return (
+    <div className="search">
+      <DateRangePicker ranges={[selectionRange]} onChange={handleSelect} />
+    </div>
+  );
 }
 
-export default Search
+export default Search;
